fb021d74ffefa1f5ea6259f6dea7b181e48f7347
msshashank1997 <139876587+msshashank1997@users.noreply.github.com>
2023-07-29 21:56:29 +0530

    Update deploybicep.ps1

--- deploybicep.ps1
+++ deploybicep.ps1
@@ -1,7 +1,7 @@
-$servicePrincipal = $env:secrets.SERVICE_PRINCIPAL_APPID }}
-$servicePrincipalSecret = $env:secrets.SERVICE_PRINCIPAL_SECRET }} 
-$servicePrincipalTenantId = $env:secrets.SERVICE_PRINCIPAL_TENANTID }}
-$azureSubscriptionName = $env:secrets.AZURE_SUBSCRIPTION_NAME }}
+$servicePrincipal = $env:secrets.SERVICE_PRINCIPAL_APPID
+$servicePrincipalSecret = $env:secrets.SERVICE_PRINCIPAL_SECRET
+$servicePrincipalTenantId = $env:secrets.SERVICE_PRINCIPAL_TENANTID
+$azureSubscriptionName = $env:secrets.AZURE_SUBSCRIPTION_NAME
 $serverName = "gihtubactions"
   
 $DID = 'DepID'
