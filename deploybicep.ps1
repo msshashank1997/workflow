b21940c498682f11e97d94591ae03c35162a671a
msshashank1997 <139876587+msshashank1997@users.noreply.github.com>
2023-07-31 10:39:15 +0530

    Update deploybicep.ps1

--- deploybicep.ps1
+++ deploybicep.ps1
@@ -2,32 +2,29 @@ $servicePrincipal = $env:secrets.SERVICE_PRINCIPAL_APPID
 $servicePrincipalSecret = $env:secrets.SERVICE_PRINCIPAL_SECRET
 $servicePrincipalTenantId = $env:secrets.SERVICE_PRINCIPAL_TENANTID
 $azureSubscriptionName = $env:secrets.AZURE_SUBSCRIPTION_NAME
+$subid= $env:secrets.AZURE_SUBSCRIPTION_ID
 $serverName = "gihtubactions"
 $adminLogin = $env:secrets.AZURE_SUBSCRIPTION_NAME
 $adminPassword = $env:secrets.ADMIN_PASSWORD
   
 $DID = 'DepID'
-
 $rgname = 'host-'+$DID
 
 #region Login
 # This logs into Azure with a Service Principal Account
 
 Write-Output "Logging in to Azure with a service principal..."
-az login `
-    --service-principal `
-    --username $servicePrincipal `
-    --password $servicePrincipalSecret `
-    --tenant $servicePrincipalTenantId
-Write-Output "Done"
-Write-Output ""
+
+# Authenticate using the service principal (clientId and clientSecret)
+$SecureClientSecret = ConvertTo-SecureString $servicePrincipalSecret -AsPlainText -Force
+$psCred = New-Object System.Management.Automation.PSCredential($servicePrincipal, $servicePrincipalSecret)
+Connect-AzAccount -ServicePrincipal -Credential $psCred -Tenant $servicePrincipalTenantId
 
 #region Subscription
 #This sets the subscription the resources will be created in
 
 Write-Output "Setting default azure subscription..."
-az account set `
-    --subscription $azureSubscriptionName
+Set-AzContext -SubscriptionId $subid
 Write-Output "Done"
 Write-Output ""
 #endregion
@@ -36,11 +33,9 @@ Write-Output ""
 # This creates the resource group used to house the VM
 
 Write-Output "Creating resource group $rgname in region eastus..."
-az group create `
-    --name 'bicep' `
-    --location "eastus"
-    Write-Output "Done creating resource group"
-    Write-Output ""
+New-AzResourceGroup -Name $rgname -Location "eastus"
+Write-Output "Done creating resource group"
+Write-Output ""
 
 #region Create VM
 # Create a VM in the resource group
